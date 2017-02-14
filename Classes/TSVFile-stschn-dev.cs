0e36cd50d1ffa71897850f11923604a96d70ced3
Steven Schneider <stschn@microsoft.com>
2017-02-14 14:50:07 -0800

    another commit

---
+++ Classes/TSVFile-stschn-dev.cs
@@ -0,0 +1,18 @@
+class TsvFile
+{
+// class variables
+    private string TsvFileName;
+
+// class constructor
+    /*public void TsvFile()
+    {
+
+    }
+    */
+
+// class private set functions
+
+// class public get functions
+
+
+}
--- Program.cs
+++ Program.cs
@@ -17,13 +17,50 @@ namespace ConsoleApplication
             bool pidStat = myNewCfg.getImportPidStat();
             string[] pidFilter = myNewCfg.getPidStatFilter();
 
-            if(ioStat)
-            {
-                Type1 type1 = new Type1("asdf");
-            }
+            
+            
+            if (ioStat) {convertIoStatToTsv();}
+            if (mpStat) {convertMpStatToTsv();}
+            if (memFree) {convertMemFreeToTsv();}
+            if (memSwap) {convertMemSwapToTsv();}
+            if (pidStat) {convertPidStatToTsv(pidFilter);}
 
             
             Console.WriteLine("Hello World!");
+        
+        }// END Main
+        public static void convertIoStatToTsv()
+        {
+            string outIoStatFileName = "*iostat.out";
+            Type1 type1OutFile = new Type1(outIoStatFileName);
+            
+
+
         }
-    }
-}
+
+        public static void convertMpStatToTsv()
+        {
+            string outMpStatFileName = "*mpstats_cpu.out";
+
+        }
+
+        public static void convertMemFreeToTsv()
+        {
+            string outMemFreeFileName = "*memory_free.out";
+
+        }
+
+        public static void convertMemSwapToTsv()
+        {
+            string outMemSwapFileName = "*memory_swap.out";
+
+        }
+        
+        public static void convertPidStatToTsv(string[] filter)
+        {
+            string outPidStatFileName = "*pidstat.out";
+
+        }
+        
+    }// END Program
+}// END Namespace ConsoleApplication
